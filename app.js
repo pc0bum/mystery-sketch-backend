9d3d62a7aac33370242ac492acc797c5e0d0d1f3
usnimoj <jominsu0103@naver.com>
2024-01-08 14:04:49 +0900

    feat: socket 기본 통신구현

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const http = require("http");
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const socketIO = require("socket.io");
 dotenv.config();
 
 const routes = require("./src/routes");
 const app = express();
+const server = http.createServer(app); // server 객체를 먼저 정의
+const io = socketIO(server);
 
 app.use(cors());
 app.use(express.json());
@@ -16,7 +19,10 @@ app.get("/ping", (req, res) => {
   res.json({ message: "pong" });
 });
 
-const server = http.createServer(app);
+io.on("connection", (socket) => {
+  console.log(`Socket connected: ${socket.id}`);
+});
+
 const PORT = process.env.TYPEORM_SERVER_PORT;
 
 const start = async () => {
