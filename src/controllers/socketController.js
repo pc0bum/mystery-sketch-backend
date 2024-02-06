6f4424b4f23720a5ef28c72f9cf7eb468c885bff
usnimoj <jominsu0103@naver.com>
2024-02-06 14:01:22 +0900

    feat: 실시간 유저 업데이트  기능 구현

--- src/controllers/socketController.js
+++ src/controllers/socketController.js
@@ -10,8 +10,23 @@ const setupSocket = (io) => {
       messages.push(data);
       io.emit("message", data);
     });
+    // socket 그림 그리기
     socket.on("draw", (data) => {
       io.emit("draw", data.x, data.y);
+    });
+    // socket 유저 업데이트
+    socket.on("newUserJoined", async ({ roomId }) => {
+      console.log("newUserJoined event received with roomId:", roomId);
+      console.log("Fetching gameroomInfo...");
+
+      try {
+        const gameroomInfo = await gameRoomDao.getGameroomInfo(roomId);
+        console.log("Fetched gameroomInfo:", gameroomInfo);
+        // 업데이트된 유저 정보를 다른 모든 클라이언트에게 브로드캐스팅
+        io.emit("userListUpdate", gameroomInfo);
+      } catch (error) {
+        console.error("Error fetching gameroomInfo:", error);
+      }
     });
   });
 };
